6909f38758a7fd10ba5dff3f4dfe89406cbf3006
DX <virajv2k@gmail.com>
2017-08-14 07:13:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying $dir patches..."
-	git apply $rootdirectory/android_device_coolpad_CP8676_I02/patches/$dir/*.patch
+	git apply $rootdirectory/device/coolpad/CP8676_I02/patches/$dir/*.patch
 	echo " "
 done
 
